5c8c7b0b119c959bc7a3b31cc152f3b7ca88811d
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-10-10 17:49:26 +0200

    echo hello world

---
+++ index.js
@@ -0,0 +1,81 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((game) => game.id !== args.id);
+
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 1000).toString(),
+      };
+      db.games.push(game);
+
+      return game;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((game) => {
+        if (game.id === args.id) return { ...game, ...args.edits };
+
+        return game;
+      });
+    },
+  },
+};
+// server setup
+const server = new ApolloServer({
+  // typeDefs -- definations of types of data
+  typeDefs,
+  // resolvers
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server ready at port", 4000);
